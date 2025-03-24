486ce42d2527506450c11ce5407490a47d2b8722
Aswarshah <johansmith.bn0753@gmail.com>
2025-03-24 11:56:43 +0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCWxGEAYSmqUFXhC-5yp-Y8PtGKHQ_oxFM",
+  authDomain: "eid-gift-fe0f6.firebaseapp.com",
+  projectId: "eid-gift-fe0f6",
+  storageBucket: "eid-gift-fe0f6.firebasestorage.app",
+  messagingSenderId: "464963301865",
+  appId: "1:464963301865:web:7ec0ae86fd310e09efdb78"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
